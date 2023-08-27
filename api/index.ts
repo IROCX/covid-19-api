136228234a222f4b4097e7d26233de791f526d5b
iroc <iroc>
2023-08-27 17:59:00 +0530

    Revert stop maintenance

--- api/index.ts
+++ api/index.ts
@@ -1,38 +1,45 @@
 import { NowResponse } from "@now/node";
 
+import {
+  getTotalConfirmed,
+  getTotalRecovered,
+  getTotalDeaths,
+  getLastUpdate
+} from "../util/api";
+
 export default async (_, response: NowResponse) => {
-  // const [confirmed, recovered, deaths, lastUpdate] = await Promise.all([
-  //   getTotalConfirmed(),
-  //   getTotalRecovered(),
-  //   getTotalDeaths(),
-  //   getLastUpdate()
-  // ]);
+  const [confirmed, recovered, deaths, lastUpdate] = await Promise.all([
+    getTotalConfirmed(),
+    getTotalRecovered(),
+    getTotalDeaths(),
+    getLastUpdate()
+  ]);
 
   response.json({
     confirmed: {
-      value: 0,
-      detail: "https://covid19.mathdro.id/api/confirmed",
+      value: confirmed,
+      detail: "https://covid19.mathdro.id/api/confirmed"
     },
     recovered: {
-      value: 0,
-      detail: "https://covid19.mathdro.id/api/recovered",
+      value: recovered,
+      detail: "https://covid19.mathdro.id/api/recovered"
     },
     deaths: {
-      value: 0,
-      detail: "https://covid19.mathdro.id/api/deaths",
+      value: deaths,
+      detail: "https://covid19.mathdro.id/api/deaths"
     },
     dailySummary: "https://covid19.mathdro.id/api/daily",
     dailyTimeSeries: {
       pattern: "https://covid19.mathdro.id/api/daily/[dateString]",
-      example: "https://covid19.mathdro.id/api/daily/2-14-2020",
+      example: "https://covid19.mathdro.id/api/daily/2-14-2020"
     },
     image: "https://covid19.mathdro.id/api/og",
     source: "https://github.com/mathdroid/covid19",
     countries: "https://covid19.mathdro.id/api/countries",
     countryDetail: {
       pattern: "https://covid19.mathdro.id/api/countries/[country]",
-      example: "https://covid19.mathdro.id/api/countries/USA",
+      example: "https://covid19.mathdro.id/api/countries/USA"
     },
-    status: "not maintained. DM @mathdroid if you want to take over",
+    lastUpdate
   });
 };
